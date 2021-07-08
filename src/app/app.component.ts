3b1853c3b10fcfbd642f3b9e53fd2e86f5d60308
Henry Peña <pecons1999@gmail.com>
2021-07-08 05:28:43 +0000

    7-7-21 advance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from './core/services/cart.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'happyTest';
+export class AppComponent implements OnInit {
+  
+
+  constructor(public cart : CartService){
+
+  }
+
+  ngOnInit(){
+
+  }
 }
